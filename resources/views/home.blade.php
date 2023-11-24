412271a0bffcf64cca83f25f2c38e632db39a7fb
ArdyJayaHermansyah <ardyjayah12@gmail.com>
2023-11-24 09:56:48 +0700

    Menambahkan tugas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div><h1>welcome</h1></div>
+    <a href="/barang">Home</a>
+    <a href="/barang/create">Create</a>
+    <a href="Read">Read</a>
+    <a href="Update">Update</a>
+</body>
+</html>
